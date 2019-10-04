aa2f58f674585c2687202a81cd754ef7f474cc4b
AngelineBrocq <38132631+AngelineBrocq@users.noreply.github.com>
2019-10-04 22:18:28 +0200

    Update ParticleBuoyancy.cpp
    Make calculations only with Z

--- MathPhys/ParticleBuoyancy.cpp
+++ MathPhys/ParticleBuoyancy.cpp
@@ -10,17 +10,18 @@ ParticleBuoyancy::ParticleBuoyancy(int p_MaxDepth, int p_ObjectVolume, int p_Wat
 
 void ParticleBuoyancy::updateForce(Particle* p_Particle, float p_Duration)
 {
-	double d = (p_Particle->getPosition() - m_WaterHeight - m_MaxDepth) / 2 * m_MaxDepth;
+
+	double d = (p_Particle->getPosition().getz() - m_WaterHeight - m_MaxDepth) / 2 * m_MaxDepth;
 
 	if (d <= 0) {
-		p_Particle->addForce(Vecteur3D(0,0,0));
+		p_Particle->addForce(Vector3D(0,0,0));
 	}
 	else if (d >= 1)
 	{
-		p_Particle->addForce(m_ObjectVolume * m_Density);
+		p_Particle->addForce(Vector3D(0, 0, m_ObjectVolume * m_Density));
 	}
 	else
 	{
-		p_Particle->addForce(d * m_ObjectVolume * m_Density);
+		p_Particle->addForce(Vector3D(0, 0, d * m_ObjectVolume * m_Density));
 	}
 }

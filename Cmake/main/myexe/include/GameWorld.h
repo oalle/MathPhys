5b5afd428f04c18f77384b6afca8240b4279e52c
Benoît RICHARD <btrd29@hotmail.fr>
2019-10-08 17:35:23 +0200

    gameworld corrections (moving non static function to static)

--- Cmake/main/myexe/include/GameWorld.h
+++ Cmake/main/myexe/include/GameWorld.h
@@ -1,21 +1,14 @@
 #pragma once
 #include "Particle.h"
-/*#include <GL/glut.h>
-#include <GL/gl.h>
-#include <GL/glu.h>*/
+#include "../../../external/freeglut/include/GL/freeglut_std.h"
 #include <iostream>
 #include <vector>
 #include "RegistreForces.h"
-#include "../../../external/freeglut/include/GL/freeglut_std.h"
 class GameWorld
 {
 private :
 	std::vector<Particle> listParticules;
 	RegistreForces registreForces;
-
-protected :
-
-	static GameWorld* instance;
 
 public :
 	GameWorld(std::vector<Particle> p_listParticules) { listParticules = p_listParticules; }
@@ -31,11 +24,5 @@ public :
 	static void displayLoopWrapper(void);
 	static void key_pressedWrapper(unsigned char key, int x, int y);
 	static void reshapeLoopWrapper(int width, int height);
-
-	virtual void initSphereObj(float x);
-	virtual void translation(Vector3D vec1);
-	virtual void displayLoop(void);
-	virtual void key_pressed(unsigned char key, int x, int y);
-	virtual void reshapeLoop(int width, int height);
 };
 
